3139ffcd38c6639acd62e9274b841fd31367e276
Bulmaro Arellano <tomaro@outlook.es>
2021-11-01 16:28:50 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+
+print("""
+BBBBB.     A      N     N  
+B    B    A A     N N   N 
+B    B   A   A    N  N  N 
+BBBBB.   AAAAA.   N.  N      
+B    B   A.  A    N.    N
+B   B    A   A    N.    N
+BBBB     A   A.   N.    N
+""")
+
+
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
